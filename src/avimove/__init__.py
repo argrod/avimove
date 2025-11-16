e05a63610000ae74f69608936183355355a7f0b9
Aran Garrod <aran.garrod@gmail.com>
2025-11-16 12:30:38 +0000

    adding imports for wind and behaviour detection

--- src/avimove/__init__.py
+++ src/avimove/__init__.py
@@ -1,1 +1,9 @@
-from .forage_detect
+from .bird_tag import bird_tag
+from . import forage_detect
+from . import wind_detect
+
+__all__ = [
+    "bird_tag",
+    "forage_detect",
+    "wind_detect",
+]
